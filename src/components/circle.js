d2baffbe4a3ffb9193e9accca2281294965f7bd6
ANSHU-1010 <94850667+ANSHU-1010@users.noreply.github.com>
2021-11-26 21:49:02 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,5 +1,22 @@
 // import using require
 
+// import using require
+const Shape =require("./shape")
+
+// declare class
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
+
+     calculateArea(){
+    console.log('Area');
+     }
+  }
+    
+// export class using module.exports
+  module.exports = Circle;
+
 // declare class
 
 
